d6743a86920005facfa6dcd8f7691276427718ee
Mariia Klymenko <mashaklimenko42@gmail.com>
2017-07-27 10:04:58 +0000

    Create recursion and array for Fibonacchi numbers

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//спосіб 1
 let num1 = parseFloat(prompt("Enter number"));
 
 function fibNumber(n) {
@@ -11,4 +12,43 @@ function fibNumber(n) {
     return n2
 }
 let num = fibNumber(num1);
-document.write(num);
+document.write('First method with use loop : ' + num);
+document.write('<br>');
+
+//спосіб 2
+function fibonacchi(n) {
+    return n <= 1 ? n : fibonacchi(n - 1) + fibonacchi(n - 2);
+}
+let newNumber = fibonacchi(num1);
+document.write('Second method with use recursion : ' + newNumber);
+document.write('<br>');
+
+//спосіб 3
+var fibonacchiArray = [];
+function calcFib(num1) {
+    let n1 = 1;
+    let n2 = 1;
+    for(var i = 3; i <= num1; i++) {
+        let n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+    }
+    return n2;
+}
+fibonacchiArray.push(+num1);
+
+let newF = calcFib(num1);
+document.write('Third method with use array : ' + newF);
+
+
+
+
+
+
+
+
+
+
+
+
+
